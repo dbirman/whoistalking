6a6d4956ec9d054a2b6e94343d1cc5b1a4734ed1
Dan Birman <danbirman@gmail.com>
2019-05-16 09:17:59 -0700

    Removing code from leitner

--- logic.js
+++ logic.js
@@ -93,27 +93,3 @@ function updateNumbers() {
 
 // 	return false;
 // }
-
-
-// function openModal(id) {
-// 	console.log('Opening: ' + id);
-// 	document.getElementById(id).style.display='block';
-// }
-
-// window.onclick = function(event) { closeCheck(event); }
-// window.ontouchstart = function(event) {closeCheck(event); }
-
-// function closeCheck(event) {
-// 	target = event.target;
-// 	if ((event.target.className == "close") || (event.target.className== "modal-content-big")) {
-//     	// chain parentElements until you find the modal
-//     	var parent = event.target.parentElement;
-//     	while (parent.className!="modal") {
-//     		parent = parent.parentElement;
-//     	}
-//     	parent.style.display = "none";
-//     }
-//     if (event.target.className == "modal") {
-//     	event.target.style.display = "none";
-//     }
-// }
